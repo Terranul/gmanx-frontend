e0e593ecf3b865a6ac682b60eb286553da183322
Terranul <bfaraone920@gmail.com>
2026-08-21 21:24:54 -0700

    Initial Commit

---
+++ gmanx-frontend/gmanx_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  gmanx_frontendApp.swift
+//  gmanx-frontend
+//
+//  Created by Ben Faraone on 2026-08-21.
+//
+
+import SwiftUI
+
+@main
+struct gmanx_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gmanx-frontendTests/gmanx_frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  gmanx_frontendTests.swift
+//  gmanx-frontendTests
+//
+//  Created by Ben Faraone on 2026-08-21.
+//
+
+import Testing
+@testable import gmanx_frontend
+
+struct gmanx_frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
