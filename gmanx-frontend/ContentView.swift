443ec13f2c0622b09ab100d9375a243b92b0319d
Terranul <bfaraone920@gmail.com>
2026-08-23 16:49:27 -0700

    Init commit

--- gmanx-frontend/ContentView.swift
+++ gmanx-frontend/ContentView.swift
@@ -5,17 +5,38 @@
 //  Created by Ben Faraone on 2026-08-21.
 //
 
+import GoogleSignIn
+import GoogleSignInSwift
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    let googleAuthScopes: [String] = ["https://mail.google.com/"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            GoogleSignInButton {
+                Task {
+                    await googleSignIn()
+                }
+            }
         }
         .padding()
+    }
+    
+    func googleSignIn() async {
+        guard let rootViewController = UIApplication.shared.rootViewController else {
+            // Handle error
+            return
+        }
+        do {
+            let result = try await GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController, hint: nil, additionalScopes: googleAuthScopes)
+            print("access Token:" + result.user.accessToken.tokenString)
+            print("credentials:" + (result.user.grantedScopes?.first ?? ""))
+        } catch {
+            print("ah shit")
+        }
     }
 }
 
--- gmanx-frontend/gmanx_frontendApp.swift
+++ gmanx-frontend/gmanx_frontendApp.swift
@@ -5,6 +5,22 @@
 //  Created by Ben Faraone on 2026-08-21.
 //
 
+import GoogleSignInSwift
+
+extension UIApplication {
+  // Minimal implementation to retrieve the active root view
+  // controller for presentation. Apps presenting sign-in from deeper
+  // within an existing view hierarchy should ensure they select the
+  // appropriate view controller.
+  var rootViewController: UIViewController? {
+    let windowScene = connectedScenes
+      .compactMap { scene in scene as? UIWindowScene }
+      .first { scene in scene.activationState == .foregroundActive }
+    return windowScene?.windows.first(where: { window in window.isKeyWindow })?.rootViewController
+  }
+}
+
+import GoogleSignIn
 import SwiftUI
 
 @main
@@ -12,6 +28,10 @@ struct gmanx_frontendApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)
+                    
+                }
         }
     }
 }
